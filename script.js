32bc7d0a6be2715baeabb9db0028bff0f60c7c12
Aghakarim-dev <hemidzade1010@gmail.com>
2025-01-03 12:26:33 +0400

    add all cards and fixed navbar position properties

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const faX = document.querySelector('.fa-x')
 const mobileNav = document.querySelector('#mobileNav')
 
 function openSideBar() {
-    mobileNav.classList.toggle('right-[0%]')
+    mobileNav.classList.toggle('translate-x-[43%]')
     faBars.classList.toggle('hidden')
     faX.classList.toggle('hidden')
 }
